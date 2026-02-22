d89b4fcd3b327d0f84c72939f6f199dde59f7c59
CodeKheb <kherbinbuenaventura@gmail.com>
2026-02-22 14:08:09 +0800

    Added tview, and experimented with inputs

--- golang_TUI.go
+++ golang_TUI.go
@@ -5,10 +5,46 @@ import (
 	"os"
 
 	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
+type Contact struct {
+	firstName string
+	lastName string
+	email string
+	phoneNumber string
+	state string
+	business bool
+}
+var contacts []Contact
+var app = tview.NewApplication().EnableMouse(true)
 
 func main() {
+	box := tview.NewBox().
+		SetBorder(true).
+		SetTitle("Hello World")
 	screen, err := tcell.NewScreen()
+
+	text := tview.NewTextView().	
+		SetTextColor(tcell.ColorBlue).
+		SetText("(q) to quit").
+		SetDynamicColors(true).
+		SetTextAlign(tview.AlignCenter)
+
+	flex := tview.NewFlex().SetDirection(tview.FlexRow)
+	flex.AddItem(box, 3, 1, false)
+	flex.AddItem(text, 1, 1, false)
+
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Rune() == 113 { // 113 for letter q in ascii hehe
+			app.Stop()
+			}
+			return event
+		})
+	if err := app.SetRoot(flex, true).Run(); err != nil {
+		panic(err)
+	}
+
+
 
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -18,27 +54,27 @@ func main() {
 		fmt.Println("ERROR SCREEN:", err)
 		os.Exit(1)
 }
-	defer screen.Fini()
+//	defer screen.Fini()
 
-	options := []string{"1", "2"}
-	selected := 0
+//	options := []string{"(1)", "(2)", "(3)", "(4)"}
+//	selected := 0
 
-	for {
-		screen.Clear()
+//	for {
+//		screen.Clear()
 
-		for i, opt := range options {
-			style := tcell.StyleDefault
-			if i == selected {
-				style = style.Foreground(tcell.ColorBlue).Background(tcell.ColorBlack)
-			}
-			for x, r := range opt {
-				screen.SetContent(x, i, r, nil, style)
-			}
-		}
+//		for i, opt := range options {
+//			style := tcell.StyleDefault
+//			if i == selected {
+//				style = style.Foreground(tcell.ColorBlue).Background(tcell.ColorBlack)
+//			}
+//			for x, r := range opt {
+//				screen.SetContent(x, i, r, nil, style)
+//			}
+//		}
 
-		screen.Show()
+//		screen.Show()
 
-	}
+//	}
 
 
 	screen.Clear()
